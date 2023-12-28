13c5270eda2cf28a0f68c19fdb8c4bf6dd78a074
Matojeje <matojeje@gmail.com>
2023-12-28 19:08:45 +0100

    Score × 10

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -101,7 +101,7 @@ public class GameManager : MonoBehaviour
         if (sGameStarted && !mGameLost)
         {
             // Increment the score by elapsed time.
-            mCurrentScore += Time.deltaTime;
+            mCurrentScore += Time.deltaTime * 10;
             // Update the score text.
             GetChildNamed(scoreText, "Value").GetComponent<Text>().text = $"{(int)(mCurrentScore)}";
         }
